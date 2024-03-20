309bedb4ffae7207e14df4ce182bedb57abf86b6
Fadel Adoe <kingpo777@gmail.com>
2024-03-20 19:26:27 +0000

    booking and menu models added

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class booking(models.Model):
+    """Maps to the Booking Table"""
+    id = models.AutoField(primary_key=True,default="1")
+    name = models.CharField(max_length=150)
+    no_of_guest = models.IntegerField()
+    booking_date =models.IntegerField()
+
+class menu(models.Model):
+    ''' Model for Menu'''
+    id = models.AutoField(primary_key=True,default="1")
+    title = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=12,decimal_places=2)
+    inventory =models.IntegerField()
+
+
